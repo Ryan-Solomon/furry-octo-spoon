dbfb2c6fbd815a367abb4d0ba980fff4eca62bf1
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-28 13:29:44 -0600

    add text input and title

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import styled from 'styled-components/native';
 
-export const App = () => {
+export default function App() {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+      <Title>What would you like to focus on?</Title>
+      <TextInput />
+    </Container>
   );
-};
+}
+
+const Container = styled.View`
+  padding: 40px 20px;
+  background-color: #333;
+  flex: 1;
+`;
+
+const Title = styled.Text`
+  font-size: 20px;
+  margin-bottom: 10px;
+  color: #fff;
+`;
