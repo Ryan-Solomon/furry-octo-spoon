6cec904ad50d1c24da476a204408f2ac4c1ae4cf
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-29 04:45:39 -0600

    create useEffect in Countdown and format time

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components/native';
 
 type Props = {
@@ -7,7 +7,28 @@ type Props = {
 };
 
 export const Countdown: FC<Props> = ({ startTime, isCountingDown }) => {
-  return <Container></Container>;
+  const [time, setTime] = useState(startTime);
+  const timeIntervalId = useRef<number>();
+
+  useEffect(() => {
+    if (time <= 0) return;
+    timeIntervalId.current = setTimeout(() => {
+      setTime((t) => t - 1);
+    }, 1000);
+    return () => clearTimeout(timeIntervalId.current);
+  }, [isCountingDown]);
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds}`;
+  };
+
+  return (
+    <Container>
+      <Time>{formatTime(time)}</Time>
+    </Container>
+  );
 };
 
 const Container = styled.View``;
